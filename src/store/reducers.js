76719060931fcf73122995b2628bea54607dc5aa
Will Hackett <willhackett@me.com>
2016-12-13 13:20:49 +1000

    Counter reducer remap

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,15 @@
 import { combineReducers } from 'redux'
 import locationReducer from './location'
 
-export const makeRootReducer = (asyncReducers) => {
-  return combineReducers({
+export const makeRootReducer = (asyncReducers) => combineReducers({
     location: locationReducer,
     ...asyncReducers
-  })
-}
+});
 
 export const injectReducer = (store, { key, reducer }) => {
-  store.asyncReducers[key] = reducer
-  store.replaceReducer(makeRootReducer(store.asyncReducers))
+    let asyncStore = store;
+    asyncStore.asyncReducers[key] = reducer;
+    asyncStore.replaceReducer(makeRootReducer(asyncStore.asyncReducers))
 }
 
 export default makeRootReducer
