dc1b6cbaff208b07e7d5f79489f9fb67f74dba44
Will Hackett <will@willhackett.com>
2017-01-24 18:08:12 +1000

    Renamed module.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import { IndexLink } from 'react-router'
 import Logo from '../Logo';
 import SidebarProfileWidget from './SidebarProfileWidget'
 import SidebarMenu from './SidebarMenu'
-import SidebarMenuLink from './SideBarMenuLink'
+import SidebarMenuItem from './SidebarMenuItem'
 
 import './styles/Sidebar.scss';
 import SidebarScripts from './SidebarScripts'
@@ -36,20 +36,20 @@ class Sidebar extends Component {
           <div className="sidebar-content">
             <SidebarProfileWidget user={user}/>
             <SidebarMenu title="Menu">
-              <SidebarMenuLink link="/feed" active={this.routeActive('/feed')} iconClasses="fa fa-newspaper-o col-grey-800">News Feed</SidebarMenuLink>
-              <SidebarMenuLink link="/profile" active={this.routeActive('/profile')} iconClasses="fa fa-list col-orange-300">Profile</SidebarMenuLink>
-              <SidebarMenuLink link="/notifications" active={this.routeActive('/notifications')} iconClasses="fa fa-bell-o col-indigo-600" badgeCount={3}>Notifications</SidebarMenuLink>
-              <SidebarMenuLink link="/events" active={this.routeActive('/events')} iconClasses="fa fa-calendar-o col-red-300">Events</SidebarMenuLink>
-              <SidebarMenuLink link="/clubs" active={this.routeActive('/clubs')} iconClasses="fa fa-users">Clubs</SidebarMenuLink>
+              <SidebarMenuItem link="/feed" active={this.routeActive('/feed')} iconClasses="fa fa-newspaper-o col-grey-800">News Feed</SidebarMenuItem>
+              <SidebarMenuItem link="/profile" active={this.routeActive('/profile')} iconClasses="fa fa-list col-orange-300">Profile</SidebarMenuItem>
+              <SidebarMenuItem link="/notifications" active={this.routeActive('/notifications')} iconClasses="fa fa-bell-o col-indigo-600" badgeCount={3}>Notifications</SidebarMenuItem>
+              <SidebarMenuItem link="/events" active={this.routeActive('/events')} iconClasses="fa fa-calendar-o col-red-300">Events</SidebarMenuItem>
+              <SidebarMenuItem link="/clubs" active={this.routeActive('/clubs')} iconClasses="fa fa-users">Clubs</SidebarMenuItem>
             </SidebarMenu>
             <SidebarMenu title="My Clubs">
               {user.club_memberships.map((c, index) =>
-                <SidebarMenuLink
+                <SidebarMenuItem
                   key={`clubmenu${index}`}
                   link={`/${c.slug}`}
                   iconImage={c.profile_picture}
                   active={false}
-                >{c.name}</SidebarMenuLink>
+                >{c.name}</SidebarMenuItem>
               )}
             </SidebarMenu>
           </div>
