df0279353b49895a988c21471f04ccb6e4c8fe52
Will Hackett <will@willhackett.com>
2017-01-30 17:48:07 +1000

    Made header slimmer on non-feed pages to conserve screen space.

--- src/components/Pages/ObjectPage/ObjectPageHeader/ObjectPageHeader.jsx
+++ src/components/Pages/ObjectPage/ObjectPageHeader/ObjectPageHeader.jsx
@@ -1,22 +1,30 @@
 import React, { Component, PropTypes } from 'react';
+import cx from 'classnames';
 import './ObjectPageHeader.scss';
 
 class ObjectPageHeader extends Component {
   static propTypes = {
     name: PropTypes.string,
     location: PropTypes.string,
-    images: PropTypes.object
+    images: PropTypes.object,
+    collapsed: PropTypes.bool
   }
   renderProfileBackdrop() {
-    const { images, name } = this.props;
+    const { images, name, collapsed } = this.props;
     if (typeof images.square === 'undefined' && typeof name !== 'undefined') {
       return <div />;
     }
+    const classes = cx({
+      'media-object': true,
+      'img-rounded-corners': collapsed === false,
+      'img-light-border': collapsed === false,
+      'thumb128': collapsed === false
+    });
     return (
       <div className="media-left">
         <div className="profile-backdrop">
           <a href="#">
-            <img src={images.square} alt={name} className="media-object img-round-corners img-light-border thumb128" />
+            <img src={images.square} alt={name} className={classes} />
           </a>
         </div>
       </div>
@@ -45,13 +53,18 @@ class ObjectPageHeader extends Component {
     const bgEle = typeof background === 'undefined' ? '#2b2b2b' : `url(${background})`;
 
     const headerImageStyles = {
-      background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.5)), ${bgEle}`,
+      background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) ${this.props.collapsed ? '0%' : '50%'}, rgba(0, 0, 0, 0.5)) 100%, ${bgEle}`,
       backgroundPosition: 'center center',
       backgroundSize: 'cover'
     }
 
+    const classes = cx({
+      'objectpage-header-container': true,
+      'collapsed': this.props.collapsed
+    });
+
     return (
-      <div className="objectpage-header-container">
+      <div className={classes}>
         <div className="banner-container" style={headerImageStyles}>
           <div className="header-details">
             <div className="details-container">
