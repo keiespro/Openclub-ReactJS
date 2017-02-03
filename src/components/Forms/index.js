79df1e75a60ad202258e77970263e8881004b657
Will Hackett <will@willhackett.com>
2017-02-03 16:45:39 +1000

    New field set and field handler form elements.

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,14 +1,27 @@
+import { FormGroup, FormControl, ControlLabel, HelpBlock } from 'react-bootstrap';
 
 import FieldSet from './FieldSet'
 import Input from './Input'
 import ImageUpload from './ImageUpload'
 import DateTimeRange from './DateTimeRange'
 import TextEditor from './TextEditor'
+import HorizontalInput from './HorizontalInput'
+import FieldHandler from './FieldHandler'
+import Form from './FormContainer';
 
 export {
+  // React Bootstrap Helpers
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock,
+  // This is us
+  Form,
+  FieldHandler,
   FieldSet,
   Input,
   ImageUpload,
   DateTimeRange,
-  TextEditor
+  TextEditor,
+  HorizontalInput,
 }
