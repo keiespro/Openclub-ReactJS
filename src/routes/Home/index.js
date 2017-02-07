170f209f5f2d8ce6fb719015c39344f042404b9d
Matt Way <matt__way@hotmail.com>
2017-02-07 15:25:52 +1000

    added home (case sensitive nonsense)

---
+++ src/routes/Home/index.js
@@ -0,0 +1,15 @@
+import { browserHistory } from 'react-router'
+import { asyncRequire } from '../../utils/codesplit'
+import Home from './components/Home'
+
+export default (store) => ({
+  onEnter: (nextState, replace, cb) => {
+    // as a child this route will only run once user login stuff has been completed
+    const state = store.getState()
+    if(state.auth.token){
+      browserHistory.replace('/feed')
+    }
+    cb()
+  },
+  getComponent: asyncRequire(() => require('./containers/HomeContainer').default)
+})
