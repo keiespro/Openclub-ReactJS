76341b76d4f0b5370676b26e16ab3f856656eacc
Will Hackett <will@willhackett.com>
2017-02-09 16:43:38 +1000

    Initial styling of events page.

--- src/routes/Events/components/EventsPage.jsx
+++ src/routes/Events/components/EventsPage.jsx
@@ -30,79 +30,83 @@ class EventsPage extends Component {
       <div>
         <div className="container-overlap bg-primary p0 pb-lg pt-lg">
           <div className="text-center">
-            <h5 className="mt0">
-              <a href="#" onClick={this.toggleView.bind(this)} className="text-white">January <i className="fa fa-caret-down" /></a>
+            <h5 className="mb-xl">
+              Registered Events
             </h5>
           </div>
-          <InfiniteCalendar
-            width={'100%'}
-            height={112}
-            showHeader={false}
-            todayHelperRowOffset={2}
-            className="visible-xs visible-sm"
-            theme={{
-                textColor: {
-                    default: '#FFF',
-                    active: '#FFF'
-                },
-                selectionColor: '#559FFF',
-                todayColor: '#FFA726',
-                weekdayColor: 'none',
-                headerColor: '#448AFF',
-                floatingNav: {
-                    background: 'rgba(56, 87, 138, 0.94)',
-                    color: '#FFF',
-                    chevron: '#FFA726'
-                }
-            }}
-          />
-          <div className="p-lg" />
         </div>
         <div className="container-fluid">
           <Col xs={12} md={8}>
-            <Event date={new Date('12 Jan 2017')} attending liked title="Splended alcoholism and co"/>
-            <Event date={new Date('19 Mar 2017')} attending title="How to use OpenClub"/>
-            <Event date={new Date('11 May 2017')} attending title="Dance Party"/>
-            <h4 className="text-center">Suggested events in your network</h4>
-            <Event date={new Date('30 Jan 2017')} title="Drag Race"/>
-            <Event date={new Date('12 Feb 2017')} liked title="Lift it! 💪🏻"/>
-            <Event date={new Date('16 Mar 2017')} liked title="Annual Law Ball"/>
-            <Event date={new Date('12 Apr 2017')} title="Drinks! Drinks! Drinks!"/>
-            <Event date={new Date('29 Apr 2017')} title="Celebrating 50 Years"/>
-            <Event date={new Date('25 Dec 2017')} title="Christmas Party"/>
+            <Row>
+              <Event date={new Date('12 Jan 2017')} attending liked title="Splended alcoholism and co"/>
+              <Event date={new Date('19 Mar 2017')} attending title="Ben tries to talk to strippers on the internet"/>
+              <Event date={new Date('11 May 2017')} attending title="Dance Party"/>
+            </Row>
+            <Row>
+              <h5 className="text-center">Suggested events in your network</h5>
+              <Event suggested date={new Date('30 Jan 2017')} title="Drag Race"/>
+              <Event suggested date={new Date('12 Feb 2017')} liked title="Lift it! 💪🏻"/>
+              <Event suggested date={new Date('16 Mar 2017')} liked title="Annual Law Ball"/>
+              <Event suggested date={new Date('12 Apr 2017')} title="Drinks! Drinks! Drinks!"/>
+              <Event suggested date={new Date('29 Apr 2017')} title="Celebrating 50 Years"/>
+              <Event suggested date={new Date('25 Dec 2017')} title="Christmas Party"/>
+            </Row>
           </Col>
           <Col xsHidden smHidden md={4}>
-            <div className="card shadowed">
+            <div className="card">
               <div className="card-body m0 text-center">
                 <button className="btn btn-success btn-lg fw">
                   <i className="fa fa-plus" /> Create Event
                 </button>
               </div>
             </div>
-            <div className="card shadowed">
-              <div className="card-body p0">
-                <InfiniteCalendar
-                  width={'100%'}
-                  height={224}
-                  showHeader={false}
-                  todayHelperRowOffset={4}
-                  className="visible-md visible-lg white"
-                  theme={{
-                      textColor: {
-                          default: '#2b2b2b',
-                          active: '#FFF'
-                      },
-                      selectionColor: '#559FFF',
-                      todayColor: '#FFA726',
-                      weekdayColor: '#008fcc',
-                      headerColor: '#448AFF',
-                      floatingNav: {
-                          background: 'rgba(56, 87, 138, 0.94)',
-                          color: '#FFF',
-                          chevron: '#FFA726'
-                      }
-                  }}
-                />
+            <div className="card">
+              <div className="card-body">
+                <button className="btn btn-default btn-lg fw"><i className="fa fa-calendar" /> Add to iCal</button>
+                <p>
+                  URL for other calendars:<br />
+                <input type="text" className="form-control fw" defaultValue="https://calendar.openclub.co/c2b3847tf89c73fg783c4f78i34cg8f" />
+                </p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card-heading">
+                <h6>Categories</h6>
+              </div>
+              <div className="card-body">
+                <div className="list-unstyled">
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-picture-o" /> Art / Photography / Film</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-book" /> Books & Literature</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-handshake-o" /> Charitable & Causes</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-smile-o" /> Comedy</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-cutlery" /> Food & Drink</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-gamepad" /> Gaming</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-medkit" /> Health & Fitness</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-glass" /> Networking</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-blind" /> Religion</a>
+                  </li>
+                  <li>
+                    <a href=""><i className="fa fa-fw fa-microphone" /> Theatre & Dance</a>
+                  </li>
+
+                </div>
               </div>
             </div>
           </Col>
