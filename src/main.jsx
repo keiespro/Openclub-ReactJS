949b88e153af6961604aa4294b70ba4ad7d5f71a
Matt Way <matt__way@hotmail.com>
2017-03-01 13:23:11 +1000

    added apollo creator

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import createStore from 'store/create_store'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import createApolloClient from 'modules/apollo'
 
 const INITIAL_STATE = window.__INITIAL_STATE__
 const MOUNT_NODE = document.getElementById('root')
@@ -9,8 +10,9 @@ const MOUNT_NODE = document.getElementById('root')
 const store = createStore(INITIAL_STATE)
 const history = syncHistoryWithStore(browserHistory, store)
 const routes = createRoutes(store)
+const apolloClient = createApolloClient(store)
 
 ReactDom.render(
-  <App { store, routes, history}/>,
+  <App { store, routes, history, apolloClient }/>,
   MOUNT_NODE
 )
