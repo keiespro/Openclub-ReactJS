8758ec1a999605868deea1a986cb498d7fb4772c
Will Hackett <will@willhackett.com>
2017-03-06 17:59:40 +1000

    Server side style rendering now live.

--- src/utils/server/serverRender.jsx
+++ src/utils/server/serverRender.jsx
@@ -4,37 +4,33 @@ import Helmet from 'react-helmet';
 import { createAppScript, createTrackingScript } from './createScripts';
 import AppContainer from '../../containers/AppContainer';
 
-const createApp = (store, props) => {
-  let componentHTML = renderToString(<AppContainer store={store} {...props} server />);
-  return { componentHTML };
+const buildPage = ({ initialState, headAssets, assets, store, routes }) => {
+  let componentHTML = renderToString(<AppContainer store={store} routes={routes} server />);
+  let styles = global.__styles__.map(style => `<style id="${style.id}" type="text/css">\n${style.parts.map(part => part.css + "\n")}\n</style>`).join('\n');
+
+  return `
+      <!doctype html>
+      <html>
+        <head>
+          ${styles}
+          ${headAssets.title.toString()}
+          ${headAssets.meta.toString()}
+          ${headAssets.link.toString()}
+          ${createTrackingScript()}
+        </head>
+        <body>
+          <div id="root">${componentHTML}</div>
+          <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}</script>
+          ${createAppScript(assets)}
+        </body>
+      </html>
+    `;
 }
-console.log(__universal__, global.__universal__);
-
-const Styles = global.__universal__.reactStyles(React);
-
-const buildPage = ({ componentHTML, initialState, headAssets, assets }) => `
-  <!doctype html>
-  <html>
-    <head>
-      ${renderToString(Styles)}
-      ${headAssets.title.toString()}
-      ${headAssets.meta.toString()}
-      ${headAssets.link.toString()}
-      ${createTrackingScript()}
-    </head>
-    <body>
-      <div id="root">${componentHTML}</div>
-      <script>window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}</script>
-      ${createAppScript(assets)}
-    </body>
-  </html>
-`
 
 const setup = (store, routes, history, assets) => {
   const initialState = store.getState();
-  const { componentHTML } = createApp(store, routes);
   const headAssets = Helmet.rewind();
-  return buildPage({ componentHTML, initialState, headAssets, assets });
+  return buildPage({ initialState, headAssets, assets, store, routes });
 }
 
 export default setup;
