a33c270d0ad24030db8ad0a3ba9bd2bae704efe1
Matt Way <matt__way@hotmail.com>
2017-03-09 12:12:09 +1000

    added club creation mutation back

--- src/routes/clubs/components/CreateClub.jsx
+++ src/routes/clubs/components/CreateClub.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import { browserHistory } from 'react-router'
 import CreateClubForm from 'components/forms/CreateClubForm'
 import {
   ContentPage,
   PageHeader
 } from 'components/layout'
 
-const CreateClub = props => (
-  <ContentPage>
-    <PageHeader title="Create New Club"/>
-    <CreateClubForm/>
-  </ContentPage>
-)
+const CreateClub = props => {
 
-export default CreateClub
+  const createTheClub = values => {
+    console.log(values)
+  }
+
+  return (
+    <ContentPage>
+      <PageHeader title="Create New Club"/>
+      <CreateClubForm onSubmit={createTheClub}/>
+    </ContentPage>
+  )
+}
+
+const createMutation = gql`
+  mutation createClub($slug: String!, $club: clubInput!){
+    createClub(slug: $slug, club: $club){
+      _id
+      slug
+    }
+  }
+`
+
+const CreateClubWithApollo = graphql(createMutation)(CreateClub)
+
+export default CreateClubWithApollo
+
+export {
+  CreateClub
+}
