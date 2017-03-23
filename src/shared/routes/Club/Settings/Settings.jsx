930b81df7404c434d7da9f3fc86be4283c4d5226
Matt Way <matt__way@hotmail.com>
2017-03-23 15:06:07 +1000

    got submenus working

--- src/shared/routes/Club/Settings/Settings.jsx
+++ src/shared/routes/Club/Settings/Settings.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Menu, Icon, Row, Col } from 'antd'
 import { ContentPage, PageHeader } from 'components/layout'
 
-const Settings = props => (
-  <ContentPage>
-    <PageHeader title="Settings"/>
-  </ContentPage>
-)
+const Settings = props => {
+
+  const handleClick = e => {
+    console.log('menu item clicked')
+  }
+
+  return (
+    <ContentPage>
+      <PageHeader title="Settings"/>
+      <Row>
+        <Col xs={{span: 0}} md={{span:6}}>
+          <Menu
+            onClick={handleClick}
+            selectedKeys={['1']}
+            mode="inline"
+          >
+            <Menu.Item key="1">Membership Plans</Menu.Item>
+            <Menu.Item key="2">Bank Details</Menu.Item>
+          </Menu>
+        </Col>
+        <Col xs={{span: 24}} md={{span: 18}}>
+          <div>This is the actual page content</div>
+        </Col>
+      </Row>
+    </ContentPage>
+  )
+}
 
 export default Settings
