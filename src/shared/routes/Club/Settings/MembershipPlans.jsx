00a465a5a5cb9d1836adf973bbf73aedd9ff20fb
Matt Way <matt__way@hotmail.com>
2017-03-27 14:55:45 +1000

    setting up settings routes properly

---
+++ src/shared/routes/Club/Settings/MembershipPlans.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Button, Icon, Col } from 'antd'
+import Table from 'components/table'
+import MembershipPlanForm from 'components/forms/MembershipPlanForm'
+import { durations } from 'constants/index'
+
+const MembershipPlans = props => {
+
+  // setup dummy table data for testing
+  const data = [
+    { id: '1', name: 'Plan A', description: 'This is a plan man', prices: [
+      { duration: 'MONTHLY', price: 1500 },
+      { duration: 'WEEKLY', price: 1300 },
+      { duration: 'YEARLY', price: 1000 }
+    ]},
+    { id: '2', name: 'Second Plan', description: 'Here is another plan', prices: [
+      { duration: 'MONTHLY', price: 1500 },
+      { duration: 'YEARLY', price: 14000 }
+    ]}
+  ]
+
+  const priceColumns = [
+    { key: 'price', size: '50%', customDataRender: (table, price) => `$${price}` },
+    { key: 'duration', customDataRender: (table, duration) => durations.lookup[duration] }
+  ]
+
+  const columns = [
+    { title: 'Name', key: 'name' },
+    { title: 'Description', key: 'description' },
+    { title: 'Prices', tdclasses: 'no-padding', key: 'prices', customDataRender: (table, prices) => <Table data={prices} columns={priceColumns}/> },
+    { title: 'Actions', customDataRender: (table, cellData, rowData) => {
+      if(table.state.expandedKeys[rowData.id]){
+        return (
+          <div>
+            <Button className="btn-rightgap" type="primary" icon="save" onClick={() => table.updateExpanded({[rowData.id]: true})}>Save</Button>
+            <Button onClick={() => table.updateExpanded({[rowData.id]: false})}>Cancel</Button>
+          </div>
+        )
+      }else{
+        return (
+          <Button icon="edit" onClick={() => table.updateExpanded({[rowData.id]: true})}>Edit</Button>
+        )
+      }
+    }}
+  ]
+
+  const expander = rowData => (
+    <MembershipPlanForm form={`membership_form_${rowData.id}`} initialValues={rowData}></MembershipPlanForm>
+  )
+
+  return (
+    <Table
+      data={data}
+      columns={columns}
+      rowKey="id"
+      expandedKeys={{'1': true}}
+      expander={expander}
+    />
+  )
+}
+
+export default MembershipPlans
--- src/shared/routes/Club/Settings/Settings.jsx
+++ src/shared/routes/Club/Settings/Settings.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Menu, Button, Icon, Row, Col } from 'antd'
+import { Menu, Icon, Row, Col } from 'antd'
 import { ContentPage, PageHeader } from 'components/layout'
-import Table from 'components/table'
-import MembershipPlanForm from 'components/forms/MembershipPlanForm'
-import { durations } from 'constants/index'
+import MembershipPlans from './MembershipPlans'
 
 import './Settings.css'
 
@@ -12,48 +10,6 @@ const Settings = props => {
   const handleClick = e => {
     console.log('menu item clicked')
   }
-
-  // setup dummy table data for testing
-  const data = [
-    { id: '1', name: 'Plan A', description: 'This is a plan man', prices: [
-      { duration: 'MONTHLY', price: 1500 },
-      { duration: 'WEEKLY', price: 1300 },
-      { duration: 'YEARLY', price: 1000 }
-    ]},
-    { id: '2', name: 'Second Plan', description: 'Here is another plan', prices: [
-      { duration: 'MONTHLY', price: 1500 },
-      { duration: 'YEARLY', price: 14000 }
-    ]}
-  ]
-
-  const priceColumns = [
-    { key: 'price', size: '50%', customDataRender: (table, price) => `$${price}` },
-    { key: 'duration', customDataRender: (table, duration) => durations.lookup[duration] }
-  ]
-
-  const columns = [
-    { title: 'Name', key: 'name' },
-    { title: 'Description', key: 'description' },
-    { title: 'Prices', tdclasses: 'no-padding', key: 'prices', customDataRender: (table, prices) => <Table data={prices} columns={priceColumns}/> },
-    { title: 'Actions', customDataRender: (table, cellData, rowData) => {
-      if(table.state.expandedKeys[rowData.id]){
-        return (
-          <div>
-            <Button className="btn-rightgap" type="primary" icon="save" onClick={() => table.updateExpanded({[rowData.id]: true})}>Save</Button>
-            <Button onClick={() => table.updateExpanded({[rowData.id]: false})}>Cancel</Button>
-          </div>
-        )
-      }else{
-        return (
-          <Button icon="edit" onClick={() => table.updateExpanded({[rowData.id]: true})}>Edit</Button>
-        )
-      }
-    }}
-  ]
-
-  const expander = rowData => (
-    <MembershipPlanForm form={`membership_form_${rowData.id}`} initialValues={rowData}></MembershipPlanForm>
-  )
 
   return (
     <ContentPage>
@@ -71,13 +27,7 @@ const Settings = props => {
         </Col>
         <Col xs={{span: 24}} md={{span: 18}}>
           <div className="oc-club-settings-content">
-            <Table
-              data={data}
-              columns={columns}
-              rowKey="id"
-              expandedKeys={{'1': true}}
-              expander={expander}
-            />
+            <MembershipPlans/>
           </div>
         </Col>
       </Row>
