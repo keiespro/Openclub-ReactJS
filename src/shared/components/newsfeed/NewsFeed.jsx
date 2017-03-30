7a043f57115d68f1472dedca260f3003d1152fdd
Will Hackett <will@willhackett.com>
2017-03-30 10:56:11 +1000

    NewsFeed styles.

--- src/shared/components/newsfeed/NewsFeed.jsx
+++ src/shared/components/newsfeed/NewsFeed.jsx
@@ -1,11 +1,31 @@
 import React, { Component, PropTypes } from 'react';
+import { Layout, Row, Col } from 'antd';
+const { Content } = Layout;
+import NewsFeedPostForm from 'components/forms/NewsFeedPostForm';
+
+import './NewsFeed.scss';
 
 class NewsFeed extends Component {
   constructor(props) {
     super(props);
   }
+  handleSubmit() {
+    return true;
+  }
   render() {
-    return ();
+    return (
+      <Row gutter={16}>
+        <Col span={9}>
+          <Content className="newsfeed-container">
+            <div className="newsfeed">
+              <NewsFeedPostForm onSubmit={this.handleSubmit} />
+            </div>
+          </Content>
+        </Col>
+        <Col span={3}>Hello</Col>
+      </Row>
+    );
   }
 }
+
 export default NewsFeed;
