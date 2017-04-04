dc89f45c2cf38b5d77ecc14983c147a279a15dff
Will Hackett <will@willhackett.com>
2017-04-04 17:57:07 +1000

    Split feed and feed item.

---
+++ src/shared/components/forms/NewsFeedComment.jsx
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react'
+import { Form, FormItem, Input, Icon, Button } from 'antd'
+import './NewsFeedComment.scss'
+
+class NewsFeedComment extends Component {
+  static propTypes = {
+    form: PropTypes.object
+  }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      text: ''
+    }
+
+    this.handleInput = this.handleInput.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+  }
+  handleInput(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Form type="inline" onSubmit={this.handleSubmit}>
+          <FormItem>
+            <Input type="textarea" prefix={<Icon type="message" />} placeholder="Compose your comment..." autosize={{ minRows: 1 }} />
+          </FormItem>
+          <FormItem>
+            <Button type="primary" htmlType="submit" disabled>Comment</Button>
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const NewsFeedCommentForm = Form.create()(NewsFeedComment);
+
+export default NewsFeedCommentForm;
---
+++ src/shared/components/newsfeed/FeedItem.jsx
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react'
+import { Icon, Dropdown, Menu, Button } from 'antd'
+import cx from 'classnames'
+import NewsFeedComment from 'components/forms/NewsFeedComment'
+
+class FeedItem extends Component {
+  static propTypes = {
+    data: PropTypes.object
+  }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments_expanded: false
+    }
+
+    this.toggleComments = this.toggleComments.bind(this);
+  }
+  toggleComments() {
+    this.setState({
+      comments_expanded: !this.state.comments_expanded
+    })
+  }
+  render() {
+    const value = this.props.data;
+    const postMenu = (
+      <Menu onClick={this.postMenuClick}>
+        <Menu.Item key="report"><Icon type="dislike" /> Report</Menu.Item>
+        <Menu.Item key="delete" style={{ color: 'red' }}><Icon type="delete" /> Delete</Menu.Item>
+      </Menu>
+    );
+    return (<div className="post">
+      <div className="post-heading">
+        <div className="media">
+          <div className="creator-image">
+            <a href="">
+              <img src="" alt="" />
+            </a>
+          </div>
+          <div className="creator-title">
+            <p className="m0 text-bold">Dude's Name</p>
+            <small className="text-muted">
+              <Icon type={cx({ 'global': value.privacy === 'public', 'contacts': value.privacy === 'members' })} /> {cx({ 'Public': value.privacy === 'public', 'Members': value.privacy === 'members' })}
+            </small>
+          </div>
+        </div>
+        <div className="post-action-menu">
+          <Dropdown overlay={postMenu}>
+            <Button><Icon type="down" /></Button>
+          </Dropdown>
+        </div>
+      </div>
+      <div className="post-content">
+        <div className="p">
+          {value.text}
+          {'attachment' in value ? <div className="attachment" dangerouslySetInnerHTML={{ __html: value.attachment }} /> : null}
+        </div>
+      </div>
+      <div className="post-actions">
+        <Button type="primary"><Icon type="like-o" /> Like</Button>
+        {this.state.comments_expanded ? (
+          <Button onClick={this.toggleComments}><Icon type="message" /> Close Comments</Button>
+        ) : (
+          <Button onClick={this.toggleComments} type="primary"><Icon type="message" /> Comment</Button>
+        )}
+        <div className={cx({'hidden': this.state.comments_expanded === false})}>
+          <NewsFeedComment />
+        </div>
+      </div>
+    </div>)
+  }
+}
+export default FeedItem;
