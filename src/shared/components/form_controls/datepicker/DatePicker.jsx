82dba7d2b99f1733ca3f0e02f89e53bfc5bc046b
Will Hackett <will@willhackett.com>
2017-04-05 23:01:57 +1000

    Played around with the Date picker format.

--- src/shared/components/form_controls/datepicker/DatePicker.jsx
+++ src/shared/components/form_controls/datepicker/DatePicker.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import moment from 'moment'
 import { DatePicker as AntDatePicker } from 'antd'
 
+const dateFormat = 'DD/MM/YYYY'
+
 const DatePicker = ({ input, meta, ...rest }) => (
-  <AntDatePicker {...input } value={input.value || null} {...rest}/>
+  <AntDatePicker {...input } value={input.value || null} format={dateFormat} {...rest}/>
 )
 
 export default DatePicker
