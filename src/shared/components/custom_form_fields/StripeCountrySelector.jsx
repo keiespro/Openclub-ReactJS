5dbf9ba121bdd6f724e43f61041d56f96f98284a
Will Hackett <will@willhackett.com>
2017-04-05 18:42:39 +1000

    Updated country selector

--- src/shared/components/custom_form_fields/StripeCountrySelector.jsx
+++ src/shared/components/custom_form_fields/StripeCountrySelector.jsx
@@ -3,8 +3,21 @@ import { Field } from 'redux-form'
 import { Select } from 'components/form_controls'
 
 const countries = [
-  { key: 'AU', value: 'Australia' },
-  { key: 'US', value: 'United States' },
+  { value: 'AU', title: 'Australia' },
+  { value: 'CA', title: 'Canada' },
+  { value: 'DK', title: 'Denmark' },
+  { value: 'FI', title: 'Finland' },
+  { value: 'FR', title: 'France' },
+  { value: 'DE', title: 'Germany' },
+  { value: 'IE', title: 'Ireland' },
+  { value: 'NL', title: 'Netherlands' },
+  { value: 'NZ', title: 'New Zealand' },
+  { value: 'NO', title: 'Norway' },
+  { value: 'SG', title: 'Singapore' },
+  { value: 'ES', title: 'Spain' },
+  { value: 'CH', title: 'Switzerland' },
+  { value: 'GB', title: 'United Kingdon' },
+  { value: 'US', title: 'United States' }
 ]
 
 const StripeCountrySelector = props => (
