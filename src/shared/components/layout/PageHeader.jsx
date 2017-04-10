addc46e7b0a794d4d6cf885011fca0c87afb9fd6
Will Hackett <will@willhackett.com>
2017-04-10 13:26:13 +1000

    Settings / Landing route fixed up.

--- src/shared/components/layout/PageHeader.jsx
+++ src/shared/components/layout/PageHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'teardrop'
 
 import './PageHeader.scss'
 
-const PageHeader = ({ title, subtitle }) => (
+const PageHeader = ({ title, subtitle, link }) => (
   <div className="oc-page-header">
-    <h3>{title}</h3><small>{subtitle}</small>
+    {link ? <Link to={link}><h3>{title}</h3><small>{subtitle}</small></Link> : <div><h3>{title}</h3><small>{subtitle}</small></div>}
   </div>
 )
 
--- src/shared/routes/Club/Settings/Settings.jsx
+++ src/shared/routes/Club/Settings/Settings.jsx
@@ -1,47 +1,46 @@
 import React, { PropTypes } from 'react'
 import { Menu, Icon, Row, Col } from 'antd'
 import { ContentPage, PageHeader } from 'components/layout'
-import { Match, Miss, Redirect } from 'teardrop'
+import { Match, MatchGroup, Miss, Redirect } from 'teardrop'
 // Async pages
 import AsyncClubProfile from './ClubProfile'
 import AsyncFinancialDetails from './FinancialDetails'
 import AsyncMembershipPlans from './MembershipPlans'
+import Landing from './Landing'
 
 import './Settings.css'
 
 const Settings = ({ club, location, pattern }, { router }) => {
-
-  const routeDefs = [
-    { key: 'profile', render: routerProps => <AsyncClubProfile {...routerProps} club={club}/> },
-    { key: 'membership-plans', render: routerProps => <AsyncMembershipPlans {...routerProps} club={club}/> },
-    { key: 'financial-details', render: routerProps => <AsyncFinancialDetails {...routerProps} club={club} /> }
-  ]
-
   const handleClick = e => {
     router.transitionTo(`/${club.slug}/settings/${e.key}`)
   }
-
-  const postPath = location.pathname.substr(pattern.length + 1)
-  const selectedPath = routeDefs.map(r => r.key).filter(key => postPath.startsWith(key))
+  const match = location.pathname ? location.pathname.match(/^.*\/.*\/([\d\w-_]+)\/?/)[1] : '';
+  const selectedKeys = [match];
 
   return (
     <ContentPage>
-      <PageHeader title="Settings"/>
       <Row>
-        <Col xs={{span: 0}} md={{span:6}}>
+        <Col xs={{span: 0}} md={{span: 6}}>
           <Menu
             onClick={handleClick}
-            selectedKeys={selectedPath}
+            selectedKeys={selectedKeys}
             mode="inline"
           >
+          <Menu.ItemGroup key="sub1" title={<span>Club Settings</span>}>
             <Menu.Item key="profile">Profile</Menu.Item>
-            <Menu.Item key="membership-plans">Membership Plans</Menu.Item>
-            <Menu.Item key="financial-details">Financial Details</Menu.Item>
+            <Menu.Item key="plans">Membership Plans</Menu.Item>
+            <Menu.Item key="payments">Financial Details</Menu.Item>
+          </Menu.ItemGroup>
           </Menu>
         </Col>
         <Col xs={{span: 24}} md={{span: 18}}>
           <div className="oc-club-settings-content">
-            {routeDefs.map(r => <Match key={r.key} pattern={r.key} render={r.render}/>)}
+            <MatchGroup>
+              <Match pattern={`/${club.slug}/settings`} render={routerProps => <Landing {...routerProps} club={club} />} />
+              <Match pattern="profile" render={routerProps => <AsyncClubProfile {...routerProps} club={club} />} />
+              <Match pattern="plans" render={routerProps => <AsyncMembershipPlans {...routerProps} club={club} />} />
+              <Match pattern="payments" render={routerProps => <AsyncFinancialDetails {...routerProps} club={club} />} />
+            </MatchGroup>
           </div>
         </Col>
       </Row>
