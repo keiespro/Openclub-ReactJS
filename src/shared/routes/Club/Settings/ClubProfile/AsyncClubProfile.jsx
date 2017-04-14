038069e31234dfaf2a8c1abda57eb85a707ad6b4
Will Hackett <will@willhackett.com>
2017-04-14 19:43:24 +1000

    Updated webpack to newer, faster, less shit version. Updated more async components.

--- src/shared/routes/Club/Settings/ClubProfile/AsyncClubProfile.jsx
+++ src/shared/routes/Club/Settings/ClubProfile/AsyncClubProfile.jsx
@@ -1,18 +1,9 @@
-import React from 'react'
 import { asyncComponent } from 'react-async-component'
-import Error from 'components/Error/Error'
-import Loading from 'components/Loading/Loading'
+import asyncExtensions from 'utils/asyncExtensions'
 
-export default process.env.NODE_ENV === 'production' ? asyncComponent({
-  resolve: () => new Promise(resolve =>
-    require.ensure(
-      [],
-      (require) => {
-        resolve(require('./ClubProfile'));
-      },
-      'ClubProfile'
-    )
-  ),
-  ErrorComponent: ({ error }) => <Error error={error} />,
-  LoadingComponent: () => <Loading />
-}) : require('./ClubProfile').default
+export default asyncComponent({
+  async resolve() {
+    return System.import('routes/Club/Settings/ClubProfile/ClubProfile');
+  },
+  ...asyncExtensions
+})
