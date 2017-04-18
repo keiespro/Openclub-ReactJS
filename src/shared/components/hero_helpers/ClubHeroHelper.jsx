d66e386369b39d98c3aa3c92ee8bd7f56fee4803
Will Hackett <will@willhackett.com>
2017-04-18 11:23:24 +1000

    Club Hero Helper now goes away when stuff is setup.

--- src/shared/components/hero_helpers/ClubHeroHelper.jsx
+++ src/shared/components/hero_helpers/ClubHeroHelper.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import Steps, { Step } from 'antd/lib/steps'
 import { ContentPage } from 'components/layout'
 
 class ClubHeroHelper extends Component {
+  static propTypes = {
+    club: PropTypes.object
+  }
   render() {
     const { club } = this.props;
     let step
-    if(!club.details){
+    if (!club.details) {
       step = 0
-    }else if(!club.membership_plans || club.membership_plans.length <= 0){
+    } else if (!club.membership_plans || club.membership_plans.length <= 0) {
       step = 1
-    }else{
+    } else if (!club.stripe_account || !club.stripe_account.data || !club.stripe_account.data.external_accounts.data || !club.stripe_account.data.external_accounts.data.length === 0) {
       step = 2
+    } else {
+      return null;
     }
     // TODO: bank details setup
 
     return (
       <ContentPage>
         <Steps current={step}>
-          <Step title="Club Profile" description="Complete your club profile"/>
-          <Step title="Membership Plans" description="Create membership plans so that members can join"/>
-          <Step title="Bank Details" description="Add bank information so that you can receive payments"/>
+          <Step title="Club Profile" description="Complete your club profile" />
+          <Step title="Membership Plans" description="Create membership plans so that members can join" />
+          <Step title="Bank Details" description="Add bank information so that you can receive payments" />
         </Steps>
       </ContentPage>
     )
