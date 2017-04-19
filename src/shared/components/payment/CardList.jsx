6065c767d4039def6753cc9aa09edcacb1ea5778
Will Hackett <will@willhackett.com>
2017-04-19 16:36:32 +1000

    Fixed card list

--- src/shared/components/payment/CardList.jsx
+++ src/shared/components/payment/CardList.jsx
@@ -1,6 +1,8 @@
-import React, { Component, PropTypes } from 'react';
-import cx from 'classnames';
-import Table from 'antd/lib/table';
+import React, { Component, PropTypes } from 'react'
+import ds from 'deli-space'
+import cx from 'classnames'
+
+import './CardList.scss'
 
 class CardList extends Component {
   static propTypes = {
@@ -10,40 +12,40 @@ class CardList extends Component {
   constructor(props) {
     super(props)
   }
-  brandLogo(card) {
-    const brandClasses = cx({
-      'fa': true,
-      'fa-cc-visa': card.brand === 'Visa',
-      'fa-cc-masteracard': card.brand === 'MasterCard',
-      'fa-cc-amex': card.brand === 'American Express',
-      'fa-cc-discover': card.brand === 'Discover',
-      'fa-cc-jcb': card.brand === 'JCB',
-      'fa-cc-diners-club': card.brand === 'Diners Club',
-      'fa-credit-card': card.brand === 'Unknown',
-    })
-    return <i className={brandClasses} />
-  }
-  numberFormat(card) {
-    switch (card.brand) {
-      case 'American Express': return <span style={{ whiteSpace: 'nowrap' }}>{`•••• •••••• •${card.last4}`}</span>;
-      case 'Diners Club': return <span style={{ whiteSpace: 'nowrap' }}>{`•••• •••••• ${card.last4}`}</span>;
-      default: return <span style={{ whiteSpace: 'nowrap' }}>{`•••• •••• •••• ${card.last4}`}</span>;
-    }
-  }
-  actions(card) {
-    return this.props.actions(card)
-  }
   render() {
-    const { actions, cards, ...rest } = this.props;
+    const { actions, cards } = this.props;
 
-    const columns = [
-      { key: 'brand', render: (table, card) => this.brandLogo(card), width: '5%' },
-      { key: 'last4', render: (table, card) => this.numberFormat(card) },
-      { key: 'expiry', render: (table, card) => <span style={{ whiteSpace: 'nowrap' }}>{card.exp_month}/{card.exp_year}</span>, width: '5%' },
-    ]
-    if (actions) columns.push({ key: 'actions', render: (table, card) => this.actions(card) })
+    const brandLogo = card => {
+      const brandClasses = cx({
+        'fa': true,
+        'fa-cc-visa': card.brand === 'Visa',
+        'fa-cc-masteracard': card.brand === 'MasterCard',
+        'fa-cc-amex': card.brand === 'American Express',
+        'fa-cc-discover': card.brand === 'Discover',
+        'fa-cc-jcb': card.brand === 'JCB',
+        'fa-cc-diners-club': card.brand === 'Diners Club',
+        'fa-credit-card': card.brand === 'Unknown',
+      })
+      return <i className={brandClasses} />;
+    }
 
-    return <Table columns={columns} dataSource={cards} showHeader={false} pagination={false} {...rest} />
+    const numberFormat = card => {
+      let format = card.brand === 'American Express' ? [4, 6, 1] : card.brand === 'Diners Club' ? [4, 6, 0] : [4, 4, 4, 0]; //eslint-disable-line
+      return <span style={{ whiteSpace: 'nowrap' }}>{ds('•', ' ', ...format)}{card.last4}</span>;
+    }
+
+    return (
+      <ul className="credit-card-list">
+        {cards.map(card => (
+            <li key={card.id} className="card-li">
+              <div className="card-logo">{brandLogo(card)}</div>
+              <div className="card-number">{numberFormat(card)}</div>
+              <div className="card-exp">{card.exp_month} / {card.exp_year}</div>
+              {actions ? actions(card) : null}
+            </li>
+        ))}
+      </ul>
+    )
   }
 }
 export default CardList;
