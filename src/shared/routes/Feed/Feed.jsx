58d98c3fc503089a2c417a56afd029601647997c
Will Hackett <will@willhackett.com>
2017-05-02 13:38:39 +1000

    Additions to feed.

--- src/shared/routes/Feed/Feed.jsx
+++ src/shared/routes/Feed/Feed.jsx
@@ -67,7 +67,7 @@ class Feed extends Component {
                   Terms of Service
                 </a> | <a href="https://www.openclub.co/legal/privacy" target="_blank">
                   Privacy Policy
-                </a> | <a href="/help" target="_blank">
+                </a> | <a href="/help" target="_blank" rel="noopener noreferrer">
                  Help
                </a>
               </div>
